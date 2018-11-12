cd8f8b37bf486423d86075372c44474b48f64955
RON LITMAN <ron@securedtouch.com>
2018-11-12 20:00:42 +0200

    Merge branch 'nadavBranch' of https://github.com/RonLitman/Elements_Statistical_Learning into ron
    # Conflicts:
    #	Netflix/general_function.py
    #	Netflix/netfilx_main.py

--- Netflix/general_function.py
+++ Netflix/general_function.py
@@ -2,6 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import Netflix.models as models
+from sklearn.metrics import mean_squared_error
+import math
+from sklearn.neighbors import KNeighborsClassifier
+import more_itertools
+import random
 
 
 def load_and_set_data():
@@ -23,8 +29,8 @@ def load_and_set_data():
     test['test_ratings_all'] = pd.read_csv('test_ratings_all.txt', delimiter=r"\s+", header=None)
     test['test_dates_all'] = pd.read_csv('test_dates_all.txt', delimiter=r"\s+", header=None)
 
-    train['train_ratings_all'].columns = movie_titles.iloc[:, 1]
-    test['test_ratings_all'].columns = movie_titles.iloc[:, 1]
+    train['train_ratings_all'].columns = movie_titles.iloc[:, ]
+    test['test_ratings_all'].columns = movie_titles.iloc[:, ]
 
     return movie_titles, train, test
 
@@ -101,34 +107,53 @@ def print_general_info(train, test):
     print('Date range in test Ratings: {} - {}'.format(int(test['test_y_date'].min()), int(test['test_y_date'].max())))
 
 
-def clean_train(train, threshold=0, minimum_values=50, test_size=0.3):
+def clean_data(train, test):
     """
-    remove users with missing data
+    remove users with variance lower then the threshold (in ratings)
+    :param test:
+    :param train:
+    :param threshold:
+    :return:
     """
 
-    x_train, x_dev, y_train, y_dev = split_train_dev(train['train_ratings_all'], train['train_y_rating'], test_size=test_size)
-    missing_data = [x_train.astype(bool).sum(axis=1) > minimum_values][0]
-    x_train = x_train[missing_data]
-    y_train = y_train[missing_data]
+    avg_dict_train = get_avg_by_year(train['train_ratings_all'])
+    avg_dict_test = get_avg_by_year(test['test_ratings_all'])
+
+    for year, value in avg_dict_train.items():
+        train['train_ratings_all']['avg_{}_{}'.format(value[1], year)] = value[0]
+
+    for year, value in avg_dict_test.items():
+        test['test_ratings_all']['avg_{}_{}'.format(value[1], year)] = value[0]
+
+    train['train_ratings_all'] = fill_missing(train['train_ratings_all'])
+    test['test_ratings_all'] = fill_missing(test['test_ratings_all'])
+
+    train['train_ratings_all'] = set_dates_feat(train, kind='train')
+    test['test_ratings_all'] = set_dates_feat(test, kind='test')
 
 
-    #  Replacing the 0  with NaN
-    x_train = x_train.replace(0, np.nan)
-    x_dev = x_dev.replace(0, np.nan)
 
-    #  Replacing the missing values with the mean rating of the user
-    x_train = x_train.apply(lambda x: x.replace(np.nan, x.mean()), axis=0)
-    x_dev = x_dev.apply(lambda x: x.replace(np.nan, x.mean()), axis=0)
+    return train, test
 
 
-    # Adding std as feature
-    x_train['std'] = x_train.std(axis=1)
-    x_dev['std'] = x_dev.std(axis=1)
-    return x_train, x_dev, y_train, y_dev
+def split_train_dev(data, labels, test, test_size=0.2, Type='random'):
+    if Type == 'knn':
+        data = data.replace(np.nan, 0)
+        knn = KNeighborsClassifier(n_neighbors=1)
+        knn.fit(data, labels)
+        index_for_dev = knn.kneighbors(test['test_ratings_all'].replace(np.nan, 0))[1]
+        index_for_dev = list(set([int(i[0]) for i in index_for_dev]))
+        # index_for_dev = []
+        # for x in range(2000):
+        #     index_for_dev.append(random.randint(1, 9999))
 
-
-def split_train_dev(data, labels, test_size=0.4):
-    x_train, x_dev, y_train, y_dev = train_test_split(data, labels, test_size=test_size)
+        index_for_train = list(set([i for i in data.index if i not in index_for_dev]))
+        x_train = data.iloc[index_for_train, :]
+        x_dev = data.iloc[index_for_dev, :]
+        y_train = labels.iloc[index_for_train, :]
+        y_dev = labels.iloc[index_for_dev, :]
+    if Type == 'random':
+        x_train, x_dev, y_train, y_dev = train_test_split(data, labels, test_size=test_size)
     return x_train, x_dev, y_train, y_dev
 
 
@@ -136,17 +161,86 @@ def scale_min_max(x):
     return (x - x.min()) / (x.max() - x.min())
 
 
-def clean_test(test, threshold=0):
-    """
-    replace 0 with user's mean rate
-    """
+def lin_model(x_train, x_test, y_train):
+    print('\n')
+    clf = models.lin_model(x_train, y_train)
+    preds = clf.predict(x_test)
 
-    test['test_ratings_all'] = test['test_ratings_all'].replace(0, np.nan)
+    return list(more_itertools.flatten(preds))
 
-    # Replacing the missing values with the mean rating of the user
-    test['test_ratings_all'] = test['test_ratings_all'].apply(lambda x: x.replace(np.nan, x.mean()), axis=0)
 
-    # Adding std as feature
-    test['test_ratings_all']['std'] = test['test_ratings_all'].std(axis=1)
+def set_column_order(df_ratings_all):
+    coulmns_order = df_ratings_all.astype(bool).sum(axis=0).sort_values(ascending=False).index.tolist()
+    df_ratings_all = df_ratings_all[coulmns_order]
+    return df_ratings_all
 
-    return test
+
+def fill_missing(df_ratings_all):
+    '''
+
+    :param df_ratings_all:
+    :return:
+    '''
+
+    # df_ratings_all = set_column_order(df_ratings_all)
+    for i in range(14, df_ratings_all.shape[1]):
+        column_name = df_ratings_all.columns[i]
+        train_temp = df_ratings_all[df_ratings_all[column_name] > 0].iloc[:, list(range(0, i))]
+        test_temp = df_ratings_all[df_ratings_all[column_name] == 0].iloc[:, list(range(0, i))]
+        y_temp = df_ratings_all[df_ratings_all[column_name] > 0].iloc[:, [i]]
+
+        fill_na = lin_model(train_temp, test_temp, y_temp)
+
+        df_ratings_all[column_name][df_ratings_all[column_name] == 0] = fill_na
+        df_ratings_all[column_name][df_ratings_all[column_name] > 5] = 5
+        df_ratings_all[column_name][df_ratings_all[column_name] < 1] = 1
+
+    return df_ratings_all
+
+
+def set_dates_feat(dict, kind='train'):
+    df_ratings_all = dict['{}_ratings_all'.format(kind)].copy()
+    df_y_date = dict['{}_y_date'.format(kind)].copy()
+    df_dates_all = dict['{}_dates_all'.format(kind)].copy()
+
+    for i in range(df_dates_all.shape[1]):
+        df_y_date[i] = df_y_date[0]
+
+    df_dates_all = df_dates_all.replace(0, np.nan)
+
+    seen_on_same_day = ((df_dates_all - df_y_date) == 0)
+
+    rating_on_same_day = pd.DataFrame(np.array(seen_on_same_day) * np.array(df_ratings_all))
+    rating_on_same_day = rating_on_same_day.replace(0, np.nan)
+    df_ratings_all['day average'] = rating_on_same_day.mean(axis=1)
+
+    df_ratings_all['movies_on_day'] = seen_on_same_day.sum(axis=1)
+    df_ratings_all['date'] = df_y_date[0]
+    return df_ratings_all
+
+
+def get_avg_by_year(df_ratings_all):
+    '''
+
+    :param df_ratings_all:
+    :return:
+    '''
+    avg_yesr_dict = {1990: 'before', 2000: 'before', 2001: 'after'}
+    df = df_ratings_all.replace(0, np.NaN).copy()
+
+    for year in avg_yesr_dict.keys():
+        if avg_yesr_dict[year] == 'before':
+            columns_avg_list = [i for i in df.columns[:99] if int(i[0]) <= year]
+        else:
+            columns_avg_list = [i for i in df.columns[:99] if int(i[0]) >= year]
+
+        avg_yesr_dict[year] = [df[columns_avg_list].mean(axis=1), avg_yesr_dict[year]]
+
+    return avg_yesr_dict
+
+
+def scale_min_max(x):
+    return (x - x.min()) / (x.max() - x.min())
+
+
+
--- Netflix/netfilx_main.py
+++ Netflix/netfilx_main.py
@@ -1,12 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import models
-from general_function import *
+import Netflix.models as models
+from Netflix.general_function import *
 from sklearn.metrics import mean_squared_error
 import math
 import xgboost as xgb
 
+
+def run_lgb_loop(dict_train, iter_time=100):
+    train = []
+    dev = []
+    for i in range(0, iter_time):
+        x_train, x_dev, y_train, y_dev = split_train_dev(dict_train['train_ratings_all'], dict_train['train_y_rating'], test, Type='random')
+        clf, mse_train_reg, mse_dev_reg = run_lgb_model(x_train, x_dev, y_train, y_dev)
+        train.append(mse_train_reg)
+        dev.append(mse_dev_reg)
+        print(i)
+        print('minimum:', min(dev), 'maximum:', max(dev), 'std:', np.std(dev), 'mean:', np.mean(dev))
 
 def run_lin_model(x_train, x_dev, y_train, y_dev):
     print('\n')
@@ -17,7 +28,7 @@ def run_lin_model(x_train, x_dev, y_train, y_dev):
     mse_dev_reg = math.sqrt(mean_squared_error(y_dev, preds_dev))
     print('RMSE on train is: {}'.format(mse_train_reg))
     print('RMSE on DEV is: {}'.format(mse_dev_reg))
-    return clf, mse_dev_reg
+    return clf, mse_train_reg, mse_dev_reg
 
 
 def run_cat(x_train, x_dev, y_train, y_dev):
@@ -31,21 +42,16 @@ def run_cat(x_train, x_dev, y_train, y_dev):
     print('RMSE on DEV is: {}'.format(mse_dev_reg))
     return clf
 
-
-def run_lgb_model(x_train, x_dev, x_unseen, y_train, y_dev, y_unseen):
+def run_lgb_model(x_train, x_dev, y_train, y_dev):
     print('\n')
     clf = models.run_lgb(x_train, x_dev, y_train, y_dev)
     preds_train = clf.predict(x_train, num_iteration=clf.best_iteration)
     preds_dev = clf.predict(x_dev, num_iteration=clf.best_iteration)
-    preds_unseen = clf.predict(x_unseen, num_iteration=clf.best_iteration)
     mse_train_reg = math.sqrt(mean_squared_error(y_train, preds_train))
     mse_dev_reg = math.sqrt(mean_squared_error(y_dev, preds_dev))
-    mse_unseen_reg = math.sqrt(mean_squared_error(y_unseen, preds_unseen))
     print('RMSE on train is: {}'.format(mse_train_reg))
     print('RMSE on DEV is: {}'.format(mse_dev_reg))
-    print('RMSE on UNSEEN is: {}'.format(mse_unseen_reg))
-    return clf
-
+    return clf, mse_train_reg, mse_dev_reg
 
 def run_xgb_model(x_train, x_dev, y_train, y_dev):
     print('\n')
@@ -56,64 +62,28 @@ def run_xgb_model(x_train, x_dev, y_train, y_dev):
     print('RMSE on DEV is: {}'.format(mse_dev_reg))
     return clf
 
-# CONST
 
 movie_titles, train, test = load_and_set_data()
-print_general_info(train, test)
+# df_train, df_test = set_df_from_metrix(train, test)
+
 train, test = clean_data(train, test)
-x_train, x_dev, y_train, y_dev = split_train_dev(train)
 
-clf = models.lin_model(x_train, y_train)
+x_train, x_dev, y_train, y_dev = split_train_dev(train['train_ratings_all'], train['train_y_rating'], test, Type='knn')
 
-preds_train = clf.predict(x_train)
-preds_dev = clf.predict(x_dev)
+clf, mse_train_reg, mse_dev_reg = run_lin_model(x_train, x_dev, y_train, y_dev)
+clf, mse_train_reg, mse_dev_reg = run_lgb_model(x_train, x_dev, y_train, y_dev)
 
-mse_train_reg = math.sqrt(mean_squared_error(y_train, preds_train))
-mse_dev_reg = math.sqrt(mean_squared_error(y_dev, preds_dev))
-
-print('RMSE on train is: {}'.format(mse_train_reg))
-print('RMSE on DEV is: {}'.format(mse_dev_reg))
-
-# clf = models.run_catbost(x_train, x_dev, y_train, y_dev)
-#
-# preds_train = clf.predict(x_train)
-# preds_dev = clf.predict(x_dev)
-#
-# mse_train_reg = math.sqrt(mean_squared_error(y_train, preds_train))
-# mse_dev_reg = math.sqrt(mean_squared_error(y_dev, preds_dev))
-#
-# print('RMSE on train is: {}'.format(mse_train_reg))
-# print('RMSE on DEV is: {}'.format(mse_dev_reg))
-
-
-clf = models.run_lgb(x_train, x_dev, y_train, y_dev)
-test = clean_test(test)
-
-mse_dev_list = []
-for i in range(5):
-    x_train, x_dev, y_train, y_dev = clean_train(train, minimum_values=80, test_size=0.3)
-    clf, mse_dev_reg = run_lin_model(x_train, x_dev, y_train, y_dev)
-    mse_dev_list.append(mse_dev_reg)
-print('The linear regression ran {} times, the avarage RMSE is {}, Max RMSE is {}'.format(i, np.mean(mse_dev_list),
-                                                                                          max(mse_dev_list)))
-
-x_dev, x_unseen, y_dev, y_unseen = split_train_dev(x_dev, y_dev, test_size=0.5)
 # clf = run_cat(x_train, x_dev, y_train, y_dev)
 
-preds_train = clf.predict(x_train, num_iteration=clf.best_iteration)
-preds_dev = clf.predict(x_dev, num_iteration=clf.best_iteration)
+# models.run_knn_cosine(x_train, x_dev, y_train, y_dev)
+# models.run_knn_manhattan(x_train, x_dev, y_train, y_dev)
 
-mse_train_reg = math.sqrt(mean_squared_error(y_train, preds_train))
-mse_dev_reg = math.sqrt(mean_squared_error(y_dev, preds_dev))
+# clf = run_xgb_model(x_train, x_dev, y_train, y_dev)
 
-print('RMSE on train is: {}'.format(mse_train_reg))
-print('RMSE on DEV is: {}'.format(mse_dev_reg))
-clf = run_lgb_model(x_train, x_dev, x_unseen, y_train, y_dev, y_unseen)
-
-# train LGB with all the data before prediction
-# x_train, x_dev, y_train, y_dev = split_train_dev(train['train_ratings_all'], train['train_y_rating'], test_size=0.3)
-# clf = run_lgb_model(x_train, x_dev, x_unseen, y_train, y_dev, y_unseen)
+# run_lgb_loop(train, iter_time=100)
 
 preds_test = clf.predict(test['test_ratings_all'], num_iteration=clf.best_iteration)
+
+
 # np.savetxt('/Users/ronlitman/Ronlitman/University/Statistic/שנה א׳ - סמט׳ א׳/למידה סטטיסטית/Netflix/preds.csv',
 #            preds_test, delimiter=",")
